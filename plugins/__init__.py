718568e8e63deab4975b311b4e10366870507dc5
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2016-05-31 21:17:08 +0200

    Port to new module format

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2015 Impera
+    Copyright 2016 Inmanta
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -13,11 +13,11 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 
-    Contact: bart@impera.io
+    Contact: code@inmanta.com
 """
 
-from impera.resources import Resource, resource, ResourceNotFoundExcpetion
-from impera.agent.handler import provider, ResourceHandler
+from inmanta.resources import Resource, resource, ResourceNotFoundExcpetion
+from inmanta.agent.handler import provider, ResourceHandler
 
 import logging, os, shlex, subprocess
 
