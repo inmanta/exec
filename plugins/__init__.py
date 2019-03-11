96fd5337105eaa849d077566e301ba3fbcb2b690
Wouter De Borger <wouter.deborger@inmanta.com>
2019-03-11 11:46:05 +0100

    improved tooling to execute commands in a shell

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -16,12 +16,20 @@
     Contact: code@inmanta.com
 """
 
-from inmanta.resources import Resource, resource
-from inmanta.agent import handler
-from inmanta import const
-
 import shlex
+import subprocess
 
+from inmanta import const
+from inmanta.agent import handler
+from inmanta.plugins import plugin
+from inmanta.resources import Resource, resource
+
+
+@plugin
+def in_shell(command: "string"):
+    """ Wrap the command such that it is executed in a shell """
+    return subprocess.list2cmdline(["sh","-c", command])
+    
 
 @resource("exec::Run", agent="host.name", id_attribute="command")
 class Run(Resource):
